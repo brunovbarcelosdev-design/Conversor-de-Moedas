b135ac6fe56ac8d60510f3095a9539a87dd46ad8
Bruno Venancio Barcelos <brunovbarcelos.dev@gamil.com>
2026-06-04 09:03:27 -0300

    iniciando js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,74 @@
+//================= Card do Conversor Principal =================//
+
+const convertButton = document.querySelector("#convertButton");
+const currencySelect2 = document.querySelector("#moedaDestino");
 
 
+function convertvalue() { 
+    
+    const currencyValueConvert = document. querySelector("#valorOriginal");
+    const currencyValueConverted = document.querySelector("#valorConvertido");
+    const inputNumberValue = document.querySelector("#valor").value;
 
+    const quotes = {
+    "USD": 5.04,
+    "EUR": 5.86,
+    "GBP": 6.79,
+    "BTC": 357765.20
+    }
 
+    const coin = currencySelect2.value;
+    const price = quotes[coin];
+    const result = inputNumberValue / price;
 
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: coin
+    }).format(result);
+    
+    currencyValueConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputNumberValue );
 
-
-
-
-
-const select = document.querySelector('select');
-const button = document.querySelector('#main-button');
-const input = document.querySelector('.main-input');
-
-function logEvent(event) {
-    console.log(event)
+    if (currencySelect2.value === "BTC") {
+    currencyValueConverted.innerHTML =
+        (inputNumberValue / quotes.BTC).toFixed(8) + " BTC";
+    }
 }
+convertButton.addEventListener('click', convertvalue);
 
-select.addEventListener('change', logEvent);
-button.addEventListener('click', logEvent);
-input.addEventListener('input', logEvent);
+
+const shortcutItems = document.querySelectorAll('.shortcut-item');
+
+function replacement (event) {
+    const buttonClicked = event.currentTarget;
+    const destino = buttonClicked.dataset.destino;
+    currencySelect2.value = destino;
+    convertvalue();
+}
+shortcutItems.forEach(item => {
+    item.addEventListener("click", replacement);
+});
+    function changeCurrency () {
+        const currencyName = document.getElementById("nomeDestino");
+        const currencyImage = document.getElementById("bandeiradestino");
+        const images = {
+            "USD": "./assets/img/dolar.png",
+            "EUR": "./assets/img/euro.png",
+            "GBP": "./assets/img/libra.png",
+            "BTC": "./assets/img/bitcoin.png"
+        }
+        currencyImage.src = images[currencySelect2.value];
+        const namesCoin = {
+            "USD": "Dólar Americano",
+            "EUR": "Euro",
+            "GBP": "Libra Esterlina",
+            "BTC": "Bitcoin"
+        }
+        currencyName.innerHTML = namesCoin[currencySelect2.value];
+        convertvalue()
+
+    }
+currencySelect2.addEventListener("change", changeCurrency);
+
